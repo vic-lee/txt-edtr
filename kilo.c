c4f1a107ee9804b115d05db5aa6fd1329c605669
vic-lee <viclee@nyu.edu>
2019-05-03 17:12:31 -0400

    Get cursor position

--- kilo.c
+++ kilo.c
@@ -81,6 +81,32 @@ char editor_read_key()
     return c;
 }
 
+int get_cursor_position(int *rows, int *cols)
+{
+    char buf[32];
+    unsigned int i = 0;
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+
+    while (i < sizeof(buf) - 1)
+    {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+        return -1;
+
+    return 0;
+}
+
 int get_window_size(int *rows, int *cols)
 {
     /**
@@ -92,14 +118,16 @@ int get_window_size(int *rows, int *cols)
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
     {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) /* Move right by 999, down by 999 */
+            return -1;
+        return get_cursor_position(rows, cols);
     }
     else
     {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
         return 0;
-    }   
+    }
 }
 
 /*** output ***/
