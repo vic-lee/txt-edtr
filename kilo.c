26d7513e8267c05fa2bed705fab1beab3118e1cd
vic-lee <viclee@nyu.edu>
2019-05-03 17:14:43 -0400

    No new line on last line

--- kilo.c
+++ kilo.c
@@ -137,7 +137,12 @@ void editor_draw_rows()
     int y;
     for (y = 0; y < E.screenrows; y++)
     {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+
+        if (y < E.screenrows - 1) /* do not draw `\r\n` on the last line */
+        {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 
