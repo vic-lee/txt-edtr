ee9914d52812f939f570a40ffc0fd5d1f263162b
vic-lee <viclee@nyu.edu>
2019-05-04 00:33:27 -0400

    Add horizontal scrolling

--- kilo.c
+++ kilo.c
@@ -44,6 +44,7 @@ struct editor_config
 {
     int cx, cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -310,6 +311,11 @@ void editor_scroll()
         E.rowoff = E.cy;
     if (E.cy >= E.rowoff + E.screenrows)
         E.rowoff = E.cy - E.screenrows + 1;
+
+    if (E.cx < E.coloff)
+        E.coloff = E.cx;
+    if (E.cx >= E.coloff + E.screencols)
+        E.coloff = E.cx - E.screencols + 1;
 }
 
 void editor_draw_rows(struct abuf *ab)
@@ -349,10 +355,12 @@ void editor_draw_rows(struct abuf *ab)
         }
         else
         {
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0)
+                len = 0;
             if (len > E.screencols)
                 len = E.screencols;
-            ab_append(ab, E.row[filerow].chars, len);
+            ab_append(ab, &E.row[filerow].chars[E.coloff], len);
         }
 
         ab_append(ab, "\x1b[K", 3); /* K: erase in line */
@@ -380,7 +388,7 @@ void editor_refresh_screen()
     editor_draw_rows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
     ab_append(&ab, buf, strlen(buf));
 
     ab_append(&ab, "\x1b[?25h", 6); /* re-display cursor */
@@ -403,10 +411,7 @@ void editor_move_cursor(int key)
         }
         break;
     case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1)
-        {
-            E.cx++;
-        }
+        E.cx++;
         break;
     case ARROW_UP:
         if (E.cy != 0)
@@ -467,6 +472,7 @@ void init_editor()
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
